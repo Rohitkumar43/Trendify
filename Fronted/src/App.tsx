52bdc0dffcf272bce3534d5359f8b2e23366e8d6
rohit kumar <kumarrohit9337@gmail.com>
2024-12-15 00:18:32 +0530

    Done

--- Fronted/src/App.tsx
+++ Fronted/src/App.tsx
@@ -1,23 +1,32 @@
 import React from 'react'
 import { BrowserRouter as Router , Routes , Route } from 'react-router-dom';
-import Home from './pages/home';
-import Search from './pages/search';
-import Cart from './pages/cart'
-import ProductDetails from './pages/product-details';
-
+import { lazy , Suspense } from 'react';
+import Loader from './components/loader';
+const Home = lazy(() => import("./pages/home"));
+const Search = lazy(() => import("./pages/search"));
+const ProductDetails = lazy(() => import("./pages/product-details"));
+const Cart = lazy(() => import("./pages/cart"));
+// const Shipping = lazy(() => import("./pages/shipping"));
+// const Login = lazy(() => import("./pages/login"));
+// const Orders = lazy(() => import("./pages/orders"));
+// const OrderDetails = lazy(() => import("./pages/order-details"));
+// const NotFound = lazy(() => import("./pages/not-found"));
+// const Checkout = lazy(() => import("./pages/checkout"));
 
 const App = () => {
   return (
     <Router>
       {/* Header */}
+        <Suspense fallback={<Loader/>}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/search" element={<Search />} />
           <Route path="/product/:id" element={<ProductDetails />} />
           <Route path="/cart" element={<Cart />} />
           {/* Not logged In Route */}
-          </Routes>
-          </Router>
+        </Routes>
+        </Suspense>
+    </Router>
   )
 }
 
